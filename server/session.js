9a30ae5ebdebee9cedda42fef3556c4bf241da7a
darksystem <darksystemcoder@gmail.com>
2023-03-29 01:32:10 +0100

    basiclly buying products is done

--- server/session.js
+++ server/session.js
@@ -56,14 +56,78 @@ wss.on('connection', function connection(ws) {
         } else if (data.type === 'createProduct') {
             var user = users[data.id][1]
             var products = database.Companies[data.company].products
-            if (user)
-                database.Users[user.Username] = user
+            var imageType;
+            if(data.image.includes('png')){
+                imageType='png'
+            }else if(data.image.includes('jpeg')){
+                imageType='jpeg'
+            }else if(data.image.includes('jpg')){
+                imageType='jpg'
+            }else{
+                ws.send('error')
+            }
+            var image ={fileName:`${data.name}PrdtImg.${imageType}`,data:data.image.replace(`data:image/${imageType};base64,`,'')}
+            fs.writeFileSync(`${__dirname.replace('/server','')}/images/Companies/${image.fileName}`, image.data, 'base64');
+            if (user.Companies.includes(data.company)){
+                products[data.name]={
+                    'name':data.name,
+                    'image':image.fileName,
+                    'description':data.description, 
+                    'price':data.price
+                }
+            }
+            database.Users[user.Username] = user
             console.log(user)
             fs.writeFileSync(`${__dirname}/data/data.json`, JSON.stringify(database))
             ws.send('done')
 
 
-        }
+        }else if (data.type === 'orderProduct') {
+            function createUuid() {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                  var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                  return v.toString(16);
+                });
+              }
+            console.log(JSON.stringify(data))
+            console.log(users)
+            var user = users[data.id][1]
+            var compName = data.name
+            //user.Stocks[compName] = 100 * (data.amount * [database.Companies[data.name].worth / database.Companies[data.name].stocks]) / database.Companies[data.name].worth
+            //database.Companies[data.name].stocks += data.amount
+            user.Balance -= data.amount * database.Companies[data.name].products[data.product].price
+            var orderId = createUuid()
+            user.orders[data.name].push({product:data.product,id:orderId})
+            database.Companies[data.name].orders[orderId]={
+                'amount':data.amount,
+                'price':data.amount * database.Companies[data.name].products[data.product].price,
+                'user':user.Username,
+                'product':data.product,
+                'status':'ordered'
+            }
+            var owner = database.Users[database.Companies[data.name].owner]
+            var notifications = owner.Notifications.Companies[compName]
+            notifications.push({
+                'type':'buyProduct',
+            'user':user.Username,'product':data.product,
+            'amount':data.amount,
+            'cost':data.amount * database.Companies[data.name].products[data.product].price
+        })
+            database.Users[user.Username] = user
+            console.log(user)
+            fs.writeFileSync(`${__dirname}/data/data.json`, JSON.stringify(database))
+            ws.send(JSON.stringify({order:orderId,status:'ordered'}))
+        }else if(data.type==='buyProduct'){
+            console.log(JSON.stringify(data))
+            console.log(users)
+            var user = users[data.id][1]
+            var compName = data.name
+            
+            database.Users[user.Username] = user
+            console.log(user)
+            fs.writeFileSync(`${__dirname}/data/data.json`, JSON.stringify(database))
+            ws.send(JSON.stringify({order:orderId,status:'delivered'}))
+        } 
     });
 
 

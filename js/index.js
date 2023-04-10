c4c6d330883f7a990bfcddad649c9a5535f78827
darksystem <darksystemcoder@gmail.com>
2023-04-10 21:18:33 +0100

    fixes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.getElementsByClassName("login")[0].addEventListener("click", function() {
-    window.location.replace(`${location}login.html`)
+    window.location.replace(`${location}/html/login.html`)
 })
 document.getElementsByClassName("signup")[0].addEventListener("click", function() {
-    window.location.replace(`${location}signup.html`)
+    window.location.replace(`${location}/html/signup.html`)
 })

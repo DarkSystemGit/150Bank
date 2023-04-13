507033eea7194aa136bbf26bf5c4c8ad80a95be1
darksystem <darksystemcoder@gmail.com>
2023-04-14 00:58:16 +0100

    End of the Day(EoD) commit

--- js/search.js
+++ js/search.js
@@ -1,23 +1,24 @@
-
-
-function search(query,items) {
-    const fuse = new Fuse(items,{keys: ["name", "description"]});  
+function search(query, items) {
+  const fuse = new Fuse(items, {
+    keys: ["name", "description"]
+  });
   if (!query) {
     return [];
   }
 
   return fuse.search(query).map((result) => result.item);
 }
-function searchResults(query,items){
-    var search = search(query,items)
-    var list = document.createElement('div')
-    list.innerHTML = `<ul id="searchResults" class="uk-list uk-list-divider"></ul>`
-    document.getElementById('main').innerHTML=""
-    document.getElementById('main').appendChild(list)
-    for(var counter=0;counter<search.length;counter++){
-        var result = document.createElement('li')
-        result.addEventListener('click')
-        var searchResult =`
+
+function searchResults(query, items) {
+  var search = search(query, items)
+  var list = document.createElement('div')
+  list.innerHTML = `<ul id="searchResults" class="uk-list uk-list-divider"></ul>`
+  document.getElementById('main').innerHTML = ""
+  document.getElementById('main').appendChild(list)
+  for (var counter = 0; counter < search.length; counter++) {
+    var result = document.createElement('li')
+    result.addEventListener('click')
+    var searchResult = `
         <img src="./../images/Companies/${search[counter].image}" style="width:14%;height:14%;"  alt="">                
                 <div style="display: block;margin-left: 1%;">
                     <h1 style="margin: 0;">${search[counter].name}</h1>
@@ -26,12 +27,43 @@ function searchResults(query,items){
                     <p style="margin: 0;">${search[counter].description}</p>
                 </div>
         `
-        var resHtml = document.createElement('div').style.display ='flex'
-        resHtml.innerHTML = searchResult
-        result.appendChild(resHtml)
-        document.getElementById('searchResults').appendChild(result)
-    }
+    var resHtml = document.createElement('div').style.display = 'flex'
+    resHtml.innerHTML = searchResult
+    result.appendChild(resHtml)
+    document.getElementById('searchResults').appendChild(result)
+  }
 }
-(async function(){
-    
-})()
+async function connection(message, url) {
+  return new Promise((resolve, reject) => {
+    var socket = new WebSocket(`ws://${url}`);
+    socket.onopen = () => {
+      socket.send(JSON.stringify(message));
+    };
+    socket.onmessage = (event) => {
+      resolve(event.data);
+    };
+    socket.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
+async function compileProductData() {
+    var products = []
+    var productList = JSON.parse(await connection({
+      type: 'listProducts'
+    }, `${location.hostname}:5002`))
+    productList.forEach(async (product) => {
+      products.append(await connection({
+        type: 'getProductData',
+        name: product
+      }, `${location.hostname}:5002`))
+    })
+    return products
+  }
+  (async function () {
+  var searchBar = document.getElementById('searchBar')
+  document.getElementById('search').addEventListener('submit',async()=>{
+    var products = await compileProductData()
+    searchResults(searchBar.value,products)
+  })
+  })()

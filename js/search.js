b37ef5c10264af9031c87a0fd78b66a64c958c04
darksystem <darksystemcoder@gmail.com>
2023-04-14 22:11:01 +0100

    collab push

--- js/search.js
+++ js/search.js
@@ -1,23 +1,27 @@
-function search(query, items) {
+
+function fuzeSearch(query, items) {
   const fuse = new Fuse(items, {
-    keys: ["name", "description"]
+    keys: ["name", "description"],
+    threshold: 0.4
   });
   if (!query) {
     return [];
   }
-
+  console.log(fuse.search(query))
   return fuse.search(query).map((result) => result.item);
 }
 
 function searchResults(query, items) {
-  var search = search(query, items)
+  var search = fuzeSearch(query, items)
+  
   var list = document.createElement('div')
   list.innerHTML = `<ul id="searchResults" class="uk-list uk-list-divider"></ul>`
   document.getElementById('main').innerHTML = ""
   document.getElementById('main').appendChild(list)
+  console.log(search)
   for (var counter = 0; counter < search.length; counter++) {
     var result = document.createElement('li')
-    result.addEventListener('click')
+    result.addEventListener('click',function(){})
     var searchResult = `
         <img src="./../images/Companies/${search[counter].image}" style="width:14%;height:14%;"  alt="">                
                 <div style="display: block;margin-left: 1%;">
@@ -53,17 +57,20 @@ async function compileProductData() {
       type: 'listProducts'
     }, `${location.hostname}:5002`))
     productList.forEach(async (product) => {
-      products.append(await connection({
+      products.push(JSON.parse(await connection({
         type: 'getProductData',
         name: product
-      }, `${location.hostname}:5002`))
+      }, `${location.hostname}:5002`)))
     })
     return products
   }
   (async function () {
   var searchBar = document.getElementById('searchBar')
-  document.getElementById('search').addEventListener('submit',async()=>{
+  console.log(searchBar)
+  document.getElementById('search').addEventListener('submit',async(e)=>{
+    e.preventDefault()
     var products = await compileProductData()
+    console.log(searchBar.value)
     searchResults(searchBar.value,products)
   })
   })()
